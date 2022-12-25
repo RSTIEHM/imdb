609af2d1c6b5f64a7fed1fbf9fa6662358d608ff
Richard Stiehm <buzzysanders@gmail.com>
2022-12-25 13:45:15 -0500

    Add Icons and logo

--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,21 @@
-
-
+import HeaderIcon from "./HeaderIcon"
+import Image from "next/image"
+import { HomeIcon, UserIcon, PhoneIcon, InformationCircleIcon } from "@heroicons/react/solid";
 const Header = () => {
   return (
-    <div>Header!!</div>
+    <div>
+        <div>
+            <HeaderIcon Icon={HomeIcon} title="HOME" />
+            <HeaderIcon Icon={UserIcon} title="USER" />
+            <HeaderIcon Icon={PhoneIcon} title="CONTACT" />
+            <HeaderIcon Icon={InformationCircleIcon} title="INFO" />
+        </div>
+        <Image 
+        src="https://upload.wikimedia.org/wikipedia/commons/6/69/IMDB_Logo_2016.svg" 
+        width={100}
+        height={100}
+        />
+    </div>
   )
 }
 
