c1e23bb8d3a0d2cc7c68051197c2fc181c96ce30
Richard Stiehm <buzzysanders@gmail.com>
2022-12-26 03:46:37 -0500

    Style the Header Section

--- components/Header.js
+++ components/Header.js
@@ -3,17 +3,18 @@ import Image from "next/image"
 import { HomeIcon, UserIcon, PhoneIcon, InformationCircleIcon } from "@heroicons/react/solid";
 const Header = () => {
   return (
-    <div>
-        <div>
+    <div className="bg-gray-700 text-gray-200 flex flex-col items-center p-6 select-none sm:flex-row justify-between">
+        <div className="flex">
             <HeaderIcon Icon={HomeIcon} title="HOME" />
             <HeaderIcon Icon={UserIcon} title="USER" />
             <HeaderIcon Icon={PhoneIcon} title="CONTACT" />
             <HeaderIcon Icon={InformationCircleIcon} title="INFO" />
         </div>
         <Image 
-        src="https://upload.wikimedia.org/wikipedia/commons/6/69/IMDB_Logo_2016.svg" 
-        width={100}
-        height={100}
+            src="https://upload.wikimedia.org/wikipedia/commons/6/69/IMDB_Logo_2016.svg" 
+            width={100}
+            height={100}
+            className="cursor-pointer active:brightness-110"
         />
     </div>
   )
