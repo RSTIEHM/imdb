b9715801597189accef42ea3dfaa50398f72d5ec
Richard Stiehm <buzzysanders@gmail.com>
2022-12-26 10:53:59 -0500

    Basic Site Complete

--- components/Results.js
+++ components/Results.js
@@ -2,7 +2,9 @@ import React from 'react'
 import Card from './Card'
 const Results = ({results}) => {
   return (
-    <div>
+    <div className='
+    bg-gray-700 text-gray-200 select-none 
+    sm:grid grid-cols-2 lg:grid grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5'>
       {results.map(result =>(
        <Card key={result.id} result={result} />
       ))}
